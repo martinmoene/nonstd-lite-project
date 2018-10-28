a75561efeb2f7934943b950448c76ad43f723896
Martin Moene <martin.moene@gmail.com>
2018-10-28 10:27:14 +0100

    Update conanfile.py to copy header to pkg/include/nonstd/

--- file/conanfile.py
+++ file/conanfile.py
@@ -1,14 +1,22 @@
 from conans import ConanFile
 
-class ValuePtrLiteConan(ConanFile):
+class XxxLiteConan(ConanFile):
     version = "0.0.0"
     name = "XXX-lite"
     description = "XXX"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
-    url = "https://github.com/martinmoene/XXX"
+    url = "https://github.com/martinmoene/XXX.git"
     exports_sources = "include/nonstd/*", "LICENSE.txt"
     build_policy = "missing"    
     author = "Martin Moene"
+
+    def build(self):
+        """Avoid warning on build step"""
+        pass
+
+    def package(self):
+        """Provide pkg/include/nonstd/*.hpp"""
+        self.copy("*.hpp")
 
     def package_info(self):
         self.info.header_only()
